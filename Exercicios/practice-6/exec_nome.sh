36731a1af66ec08441b91b58a51f5f51bf34a6e0
Heloisa Pereira <heloisa.pereira@g.globo>
2025-01-23 13:18:48 -0300

    Realizado um laço de repetição no arquivo exec_nome.sh

--- Exercicios/practice-6/exec_nome.sh
+++ Exercicios/practice-6/exec_nome.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Olá, meu nome é Heloisa!"
+#Executar meu nome 
+
+	echo "Olá, meu nome é Heloisa!"
+
+#Script para escrever numeros de 1 a 100
+
+	for numero in {1..100}; do 
+		echo "$numero" 
+	done
+ 
